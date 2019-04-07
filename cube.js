853ac40f65ad9b80419e3b8a19a8fbd4119867b7
garettridge <garett.ridge.github@gmail.com>
2019-04-06 21:54:55 -0700

    Cleaned up Angel's misleading shorthand
    No longer looks like it's indexed
    
    No longer uses odd trick for flat shading

--- cube.js
+++ cube.js
@@ -7,6 +7,90 @@ var NumVertices  = 36;
 var points = [];
 var colors = [];
 
+
+var vertexPositions = [
+    vec4( -0.5, 0.5, 0.5, 1 ),
+    vec4( -0.5, -0.5, 0.5, 1 ),
+    vec4( 0.5, -0.5, 0.5, 1 ),
+    vec4( -0.5, 0.5, 0.5, 1 ),
+    vec4( 0.5, -0.5, 0.5, 1 ),
+    vec4( 0.5, 0.5, 0.5, 1 ),
+    vec4( 0.5, 0.5, 0.5, 1 ),
+    vec4( 0.5, -0.5, 0.5, 1 ),
+    vec4( 0.5, -0.5, -0.5, 1 ),
+    vec4( 0.5, 0.5, 0.5, 1 ),
+    vec4( 0.5, -0.5, -0.5, 1 ),
+    vec4( 0.5, 0.5, -0.5, 1 ),
+    vec4( 0.5, -0.5, 0.5, 1 ),
+    vec4( -0.5, -0.5, 0.5, 1 ),
+    vec4( -0.5, -0.5, -0.5, 1 ),
+    vec4( 0.5, -0.5, 0.5, 1 ),
+    vec4( -0.5, -0.5, -0.5, 1 ),
+    vec4( 0.5, -0.5, -0.5, 1 ),
+    vec4( 0.5, 0.5, -0.5, 1 ),
+    vec4( -0.5, 0.5, -0.5, 1 ),
+    vec4( -0.5, 0.5, 0.5, 1 ),
+    vec4( 0.5, 0.5, -0.5, 1 ),
+    vec4( -0.5, 0.5, 0.5, 1 ),
+    vec4( 0.5, 0.5, 0.5, 1 ),
+    vec4( -0.5, -0.5, -0.5, 1 ),
+    vec4( -0.5, 0.5, -0.5, 1 ),
+    vec4( 0.5, 0.5, -0.5, 1 ),
+    vec4( -0.5, -0.5, -0.5, 1 ),
+    vec4( 0.5, 0.5, -0.5, 1 ),
+    vec4( 0.5, -0.5, -0.5, 1 ),
+    vec4( -0.5, 0.5, -0.5, 1 ),
+    vec4( -0.5, -0.5, -0.5, 1 ),
+    vec4( -0.5, -0.5, 0.5, 1 ),
+    vec4( -0.5, 0.5, -0.5, 1 ),
+    vec4( -0.5, -0.5, 0.5, 1 ),
+    vec4( -0.5, 0.5, 0.5, 1 )
+];
+
+var vertexColors = [
+    [ 1.0, 0.0, 0.0, 1.0 ], // red
+    [ 1.0, 0.0, 0.0, 1.0 ],
+    [ 1.0, 0.0, 0.0, 1.0 ],
+    [ 1.0, 0.0, 0.0, 1.0 ],
+    [ 1.0, 0.0, 0.0, 1.0 ],
+    [ 1.0, 0.0, 0.0, 1.0 ],
+
+    [ 1.0, 1.0, 0.0, 1.0 ], // yellow
+    [ 1.0, 1.0, 0.0, 1.0 ],
+    [ 1.0, 1.0, 0.0, 1.0 ],
+    [ 1.0, 1.0, 0.0, 1.0 ],
+    [ 1.0, 1.0, 0.0, 1.0 ],
+    [ 1.0, 1.0, 0.0, 1.0 ],
+
+    [ 0.0, 1.0, 0.0, 1.0 ], // green
+    [ 0.0, 1.0, 0.0, 1.0 ],
+    [ 0.0, 1.0, 0.0, 1.0 ],
+    [ 0.0, 1.0, 0.0, 1.0 ],
+    [ 0.0, 1.0, 0.0, 1.0 ],
+    [ 0.0, 1.0, 0.0, 1.0 ],
+
+    [ 0.0, 1.0, 1.0, 1.0 ], // cyan
+    [ 0.0, 1.0, 1.0, 1.0 ],
+    [ 0.0, 1.0, 1.0, 1.0 ],
+    [ 0.0, 1.0, 1.0, 1.0 ],
+    [ 0.0, 1.0, 1.0, 1.0 ],
+    [ 0.0, 1.0, 1.0, 1.0 ],
+
+    [ 0.0, 0.0, 1.0, 1.0 ], // blue
+    [ 0.0, 0.0, 1.0, 1.0 ],
+    [ 0.0, 0.0, 1.0, 1.0 ],
+    [ 0.0, 0.0, 1.0, 1.0 ],
+    [ 0.0, 0.0, 1.0, 1.0 ],
+    [ 0.0, 0.0, 1.0, 1.0 ],
+
+    [ 1.0, 0.0, 1.0, 1.0 ], // magenta
+    [ 1.0, 0.0, 1.0, 1.0 ],
+    [ 1.0, 0.0, 1.0, 1.0 ],
+    [ 1.0, 0.0, 1.0, 1.0 ],
+    [ 1.0, 0.0, 1.0, 1.0 ],
+    [ 1.0, 0.0, 1.0, 1.0 ]
+];
+
 var xAxis = 0;
 var yAxis = 1;
 var zAxis = 2;
@@ -23,8 +107,6 @@ window.onload = function init()
     gl = WebGLUtils.setupWebGL( canvas );
     if ( !gl ) { alert( "WebGL isn't available" ); }
 
-    colorCube();
-
     gl.viewport( 0, 0, canvas.width, canvas.height );
     gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
     
@@ -38,7 +120,7 @@ window.onload = function init()
     
     var cBuffer = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer );
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(vertexColors), gl.STATIC_DRAW );
 
     var vColor = gl.getAttribLocation( program, "vColor" );
     gl.vertexAttribPointer( vColor, 4, gl.FLOAT, false, 0, 0 );
@@ -46,7 +128,7 @@ window.onload = function init()
 
     var vBuffer = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(vertexPositions), gl.STATIC_DRAW );
     
 
     var vPosition = gl.getAttribLocation( program, "vPosition" );
@@ -70,58 +152,6 @@ window.onload = function init()
     render();
 }
 
-function colorCube()
-{
-    quad( 1, 0, 3, 2 );
-    quad( 2, 3, 7, 6 );
-    quad( 3, 0, 4, 7 );
-    quad( 6, 5, 1, 2 );
-    quad( 4, 5, 6, 7 );
-    quad( 5, 4, 0, 1 );
-}
-
-function quad(a, b, c, d) 
-{
-    var vertices = [
-        vec4( -0.5, -0.5,  0.5, 1.0 ),
-        vec4( -0.5,  0.5,  0.5, 1.0 ),
-        vec4(  0.5,  0.5,  0.5, 1.0 ),
-        vec4(  0.5, -0.5,  0.5, 1.0 ),
-        vec4( -0.5, -0.5, -0.5, 1.0 ),
-        vec4( -0.5,  0.5, -0.5, 1.0 ),
-        vec4(  0.5,  0.5, -0.5, 1.0 ),
-        vec4(  0.5, -0.5, -0.5, 1.0 )
-    ];
-
-    var vertexColors = [
-        [ 0.0, 0.0, 0.0, 1.0 ],  // black
-        [ 1.0, 0.0, 0.0, 1.0 ],  // red
-        [ 1.0, 1.0, 0.0, 1.0 ],  // yellow
-        [ 0.0, 1.0, 0.0, 1.0 ],  // green
-        [ 0.0, 0.0, 1.0, 1.0 ],  // blue
-        [ 1.0, 0.0, 1.0, 1.0 ],  // magenta
-        [ 0.0, 1.0, 1.0, 1.0 ],  // cyan
-        [ 1.0, 1.0, 1.0, 1.0 ]   // white
-    ];
-
-    // We need to parition the quad into two triangles in order for
-    // WebGL to be able to render it.  In this case, we create two
-    // triangles from the quad indices
-    
-    //vertex color assigned by the index of the vertex
-    
-    var indices = [ a, b, c, a, c, d ];
-
-    for ( var i = 0; i < indices.length; ++i ) {
-        points.push( vertices[indices[i]] );
-        //colors.push( vertexColors[indices[i]] );
-    
-        // for solid colored faces use 
-        colors.push(vertexColors[a]);
-        
-    }
-}
-
 function render()
 {
     gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
@@ -133,4 +163,3 @@ function render()
 
     requestAnimFrame( render );
 }
-
